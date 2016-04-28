39393dfd809833565377167fe28606cf4ceeb0cb
Jake Garrison <omonoid@uw.edu>
2016-04-28 02:34:24 -0700

    Initial Commit

---
+++ audio_split/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  audio_split
+//
+//  Created by Jake Garrison on 4/28/16.
+//  Copyright © 2016 Jake Garrison. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
