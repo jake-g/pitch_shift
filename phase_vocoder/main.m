155bb862526cf22cc4cbb3e1b7fff83dd2709d3e
Jake Garrison <omonoid@uw.edu>
2016-05-04 01:30:26 -0700

    added pitch shift example

--- phase_vocoder/main.m
+++ phase_vocoder/main.m
@@ -6,17 +6,32 @@ input = 'speech_files/Maple.wav' % piano
 speed_factor = 0.3;  % slow down
 fft_size = 1024;
 
-% Change Speed
+%% Change Speed
 [x,sr] = audioread(input);
-y = pvoc(x, speed_factor); 
+y = pvoc(x, speed_factor); % pvoc(input, factor)
 len = max(length(y), length(x)); % longer signal
 
-%% Play 
-sound(x, sr) 
-pause(); % press any key
-sound(y, sr)
+% Play 
+% sound(x, sr) 
+% pause(); % press any key
+% sound(y, sr)
 
 % Plot
 figure
-subplot(2,1,1); plot(x); xlim([0, len]); title('Original')
-subplot(2,1,2); plot(y); xlim([0, len]); title('Stretched')
+subplot(2,1,1); plot(x, 'r'); xlim([0, len]); title('Original')
+subplot(2,1,2); plot(y, 'b'); xlim([0, len]); title('Stretched')
+
+%% Change Pitch
+speed = pvoc(x, 0.8); % 4/5 speed
+pitch = resample(speed, 4, 5); % resample(input, numerator, denominator), change pitch
+len = max(length(speed), length(pitch)); % longer signal
+
+% Play
+% pause() % press any key
+% soundsc(x(1:length(pitch)) + pitch, sr) % overlap original with pitch shifted
+
+% Plot
+figure
+subplot(3,1,1); plot(x, 'm'); xlim([0, len]); title('Original')
+subplot(3,1,2); plot(speed, 'b'); xlim([0, len]); title('Time Shift')
+subplot(3,1,3); plot(pitch, 'g'); xlim([0, len]); title('Pitch Shift (resampled)')
