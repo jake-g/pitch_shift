d8202e1101fa955204bdc077cb7157004ef898ab
Jake Garrison <omonoid@uw.edu>
2016-05-04 01:03:31 -0700

    working demo

--- phase_vocoder/pvoc.m
+++ phase_vocoder/pvoc.m
@@ -3,8 +3,6 @@ function y = pvoc(x, r, n)
 %      x is an input sound. n is the FFT size, defaults to 1024.  
 %      Calculate the 25%-overlapped STFT, squeeze it by a factor of r, 
 %      inverse spegram.
-% 2000-12-05, 2002-02-13 dpwe@ee.columbia.edu.  Uses pvsample, stft, istft
-% $Header: /home/empire6/dpwe/public_html/resources/matlab/pvoc/RCS/pvoc.m,v 1.3 2011/02/08 21:08:39 dpwe Exp $
 
 if nargin < 3
   n = 1024;
