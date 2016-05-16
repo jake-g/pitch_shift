5a806238ae409bb59be8fe98ac9e1fb737ff0c72
Jake Garrison <omonoid@uw.edu>
2016-05-16 05:39:01 -0700

    working src code

---
+++ Pitch_Shift/pitchShiftWav.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sndfile.h>
+#include <string.h>
+#include <math.h>
+#include "PitchShift.h"
+
+#define NAME_SIZE 100
+/*
+
+Shifts pitch by given factor. Input and output are .wav files
+
+
+On Linux:
+sudo apt-get install libsndfile1
+sudo apt-get install libsndfile1-dev
+
+Build:
+run makefile
+
+Execute: ./pitchShift semitones inputFilename_with_extension
+
+Notes: libsndfile
+http://www.mega-nerd.com/libsndfile/api.html
+http://www.labbookpages.co.uk/audio/wavFiles.html
+*/
+
+void printBuffer(float *arr, long size);
+//void PitchShift(float pitchShift, long numSamps, long fftFrameSize, long osamp, float sampleRate, float *indata, float *outdata);
+void handleInput(int argc, char ** argv, long * semitones, char inFileName[], char fileExtension[]);
+
+int main(int argc, char * argv[])
+{
+
+    /* Initialize args*/
+    long buffer_size, semitones;
+    float *buf, sr, pitchShift;
+    int num, f, c;
+  	char inFileName[NAME_SIZE];      // file name without file extension
+    char fileExtension[NAME_SIZE];   // file extension of both in/output
+  	char outPath[NAME_SIZE];    // format: "inputName-out.extension"
+    char srcPath[NAME_SIZE];    // assuming input is in sound_files directory
+    SNDFILE *input, *output;
+    SF_INFO info;
+    handleInput(argc, argv, &semitones, inFileName, fileExtension);
+    sprintf(outPath, "sound_files/%s-out_(%ld).%s", inFileName, semitones, fileExtension);
+    sprintf(srcPath, "sound_files/%s.%s", inFileName, fileExtension);
+    pitchShift = pow(2., semitones/12.);
+
+
+    /* Open the WAV file. */
+    info.format = 0;
+    input = sf_open(srcPath, SFM_READ,&info);
+    if (input == NULL)
+    {
+        printf("Failed to open the file.\n");
+        exit(-1);
+    }
+
+    /* Print some of the info */
+    f = info.frames;
+    sr = info.samplerate;
+    c = info.channels;
+    buffer_size = f*c;
+    printf("Input: samplerate: %0.0lf, channels: %d, buffer_size: %ld\n",sr, c, buffer_size);
+    printf("Shifted by a factor of %f\n", pitchShift);
+
+
+    /* Allocate space for the data to be read, then read it. */
+    buf = (float *) malloc(buffer_size*sizeof(float));
+    num = sf_readf_float(input, buf, buffer_size);
+    sf_close(input);
+    printf("Read %d samples from %s\n", num, srcPath
+    );
+    // printBuffer(buf, buffer_size);
+
+    /* Call PitchShift() */
+    PitchShift(pitchShift, buffer_size, 2048, 4, sr, buf, buf);
+
+
+     /*  Open sound file for writing */
+  	output = sf_open(outPath, SFM_WRITE, &info);
+    num = sf_writef_float(output, buf, buffer_size);
+    sf_close(output);
+    printf("Wrote %d samples to %s\n", num, outPath);
+
+    return 0;
+}
+
+
+/* Print contents of data (float array 1 row per channel) */
+void printBuffer(float *arr, long size)
+{
+    for (int i = 0; i < size; i++) {
+        printf("%f ", arr[i]);
+    }
+}
+
+/* Parse input arguments */
+void handleInput(int argc, char ** argv, long * semitones, char inFileName[], char fileExtension[])
+{
+  if (argc != 3) {
+    printf("Needs 2 arguments\n");
+    exit(0);
+  }
+
+  *semitones = atoi(argv[1]);
+  char del[] = ".";
+  char *ptr;
+  ptr = strtok(argv[2], del);
+  if (ptr != NULL) {
+    strcpy(inFileName, ptr);
+    strcpy(fileExtension, strtok(NULL, del));
+  }
+}
---
+++ Pitch_Shift/realtime.c
@@ -0,0 +1,192 @@
+
+/*
+ * This program uses the PortAudio Portable Audio Library.
+ * For more information see: http://www.portaudio.com
+ * Copyright (c) 1999-2000 Ross Bencina and Phil Burk
+*/
+
+#include <stdio.h>
+#include <math.h>
+#include "portaudio.h"
+#include "PitchShift.h"
+
+
+#define SAMPLE_RATE         (44100)
+#define NUM_CHANNELS (1)
+#define PA_SAMPLE_TYPE      paFloat32
+#define FRAMES_PER_BUFFER   (64)
+typedef float SAMPLE;
+
+
+float CubicAmplifier( float input );
+void printBuffer(const float * arr);
+int main(void);
+static int fxCallback( const void *inputBuffer, void *outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags,
+                         void *userData );
+
+long semitones = 0;
+int count = 0;
+
+// Calls the effect on input buffer and returns output buffer when complete
+static int fxCallback( const void *inputBuffer, void *outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags,
+                         void *userData )
+{
+    // Port audio variables
+    SAMPLE *out = (SAMPLE*)outputBuffer;
+    const SAMPLE *in = (const SAMPLE*)inputBuffer;
+    unsigned int i;
+    (void) timeInfo; /* Prevent unused variable warnings. */
+    (void) statusFlags;
+    (void) userData;
+
+    // Pitch Shift variables
+    long buffer_size = FRAMES_PER_BUFFER;
+    float sr = SAMPLE_RATE;
+    long fftSize = 2048;
+    long osamp = 4;
+    float pitchShift = pow(2., semitones/12.);
+    int measure = 0;
+
+    // Fill input buffer
+    if( inputBuffer == NULL )
+    {
+        for( i=0; i<framesPerBuffer; i++ )
+        {
+            *out++ = 0;  /* left - silent */
+        }
+    }
+    else    // playback
+    {
+        // Buffer based effect
+        PitchShift(pitchShift, buffer_size , fftSize, osamp, sr, (float *)in, out);
+
+        // Sample based effect
+        // for( i=0; i<framesPerBuffer; i++ )
+        // {
+        //     // *out++ = FUZZ(*in++);
+        //     // *out++ = DIST(*in++);
+        //     // *out++ = *in++;     /* clean */
+        // }
+
+        // DEGUG
+        //   printBuffer(in);
+
+        // Melody
+        measure = 500;
+        if (count == measure) {
+          semitones = 0;
+          printf("Setting semitone to %ld...\n", semitones);
+        } else if (count == measure * 1/4) {
+          semitones += 3;
+          printf("Setting semitone to %ld...\n", semitones);
+        } else if (count == measure * 2/4) {
+          semitones += 4;
+          printf("Setting semitone to %ld...\n", semitones);
+        } else if (count == measure * 3/4) {
+          semitones += -7;
+          printf("Setting semitone to %ld...\n", semitones);
+        }
+
+    }
+    count = (count + 1) % measure;
+    // printf("%d\n", count );
+    return paContinue;
+}
+
+/*******************************************************************/
+int main(int argc, char * argv[])
+{
+    PaStreamParameters inputParameters, outputParameters;
+    PaStream *stream;
+    PaError err;
+
+    err = Pa_Initialize();
+    if( err != paNoError ) goto error;
+
+    inputParameters.device = Pa_GetDefaultInputDevice(); /* default input device */
+    if (inputParameters.device == paNoDevice) {
+      fprintf(stderr,"Error: No default input device.\n");
+      goto error;
+    }
+    inputParameters.channelCount = NUM_CHANNELS;       /* stereo input */
+    inputParameters.sampleFormat = PA_SAMPLE_TYPE;
+    inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
+    inputParameters.hostApiSpecificStreamInfo = NULL;
+
+    outputParameters.device = Pa_GetDefaultOutputDevice(); /* default output device */
+    if (outputParameters.device == paNoDevice) {
+      fprintf(stderr,"Error: No default output device.\n");
+      goto error;
+    }
+    outputParameters.channelCount = NUM_CHANNELS;       /* stereo output */
+    outputParameters.sampleFormat = PA_SAMPLE_TYPE;
+    outputParameters.suggestedLatency = Pa_GetDeviceInfo( outputParameters.device )->defaultLowOutputLatency;
+    outputParameters.hostApiSpecificStreamInfo = NULL;
+
+    err = Pa_OpenStream(
+              &stream,
+              &inputParameters,
+              &outputParameters,
+              SAMPLE_RATE,
+              FRAMES_PER_BUFFER,
+              0, /* paClipOff, */  /* we won't output out of range samples so don't bother clipping them */
+              fxCallback,
+              NULL );
+    if( err != paNoError ) goto error;
+
+    err = Pa_StartStream( stream );
+    if( err != paNoError ) goto error;
+
+    printf("Hit ENTER to stop program.\n");
+    getchar();
+    err = Pa_CloseStream( stream );
+    if( err != paNoError ) goto error;
+
+    printf("Finished...\n");
+    Pa_Terminate();
+    return 0;
+
+error:
+    Pa_Terminate();
+    fprintf( stderr, "An error occured while using the portaudio stream\n" );
+    fprintf( stderr, "Error number: %d\n", err );
+    fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
+    return -1;
+}
+
+/* Print contents of data (float array 1 row per channel) */
+void printBuffer(const float *arr)
+{
+    printf("fresh buffer...\n");
+    for (int i = 0; i < FRAMES_PER_BUFFER; i++) {
+        printf("%f ", arr[i]);
+    }
+    printf("\n");
+
+}
+
+/* Non-linear amplifier with soft distortion curve. */
+#define FUZZ(x) CubicAmplifier(CubicAmplifier(CubicAmplifier(CubicAmplifier(x))))
+#define DIST(x) CubicAmplifier(x)
+float CubicAmplifier( float input )
+{
+    float output, temp;
+    if( input < 0.0 )
+    {
+        temp = input + 1.0f;
+        output = (temp * temp * temp) - 1.0f;
+    }
+    else
+    {
+        temp = input - 1.0f;
+        output = (temp * temp * temp) + 1.0f;
+    }
+
+    return output;
+}
