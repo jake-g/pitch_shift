c5a6715f0d6a342618547df6176d6e06307e0869
Jake Garrison <omonoid@uw.edu>
2016-05-31 03:53:43 -0700

    simplified

--- delay-line/delay_line.m
+++ delay-line/delay_line.m
@@ -1,14 +1,13 @@
+%% Pitch Shift
 input_file = 'sine-wave.wav';
-sign = 1; % 1 is up 0 is down;
-semitones = -4;
-
+semitones = 4;
 output = pitch_shift(input_file, semitones);
 
-
+%% See Results
 [input, Fs]= audioread(input_file);
-% output = audioread('../sound_files/Maple_short-out_(5).wav');
 showFFT(input, output, Fs)
 
+%% Play Results (press key to end pause)
 soundsc(input, Fs)
 pause()
 soundsc(output, Fs)
